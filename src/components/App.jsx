d519e180cc825f610012e699ebd8ac09afbce405
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-06-05 23:33:07 +0200

    Added
    Form, Contacts, Section,

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Form from './Form/Form';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import Contacts from './Contacts/Contacts';
+import Section from './Section/Section';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson' },
+      { id: 'id-2', name: 'Hermione Kline' },
+      { id: 'id-3', name: 'Eden Clements' },
+      { id: 'id-4', name: 'Annie Copeland' },
+    ],
+    name: '',
+  };
+
+  addContact = event => {
+    event.preventDefault();
+    const contactCard = {
+      id: nanoid(),
+      name: this.state.name,
+    };
+    const newContacts = [...this.state.contacts, contactCard];
+    this.setState({ contacts: newContacts });
+    this.setState({ name: '' });
+  };
+
+  inputHandler = event => {
+    const newValue = event.target.value;
+    this.setState({ name: newValue });
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+    const addContact = event => this.addContact(event);
+    const inputHandler = event => this.inputHandler(event);
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <Form
+            name={name}
+            addContact={addContact}
+            inputHandler={inputHandler}
+          />
+        </Section>
+        <Section title="Contacts">
+          <Contacts  contacts={contacts} />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import css from './Contacts.module.css'
+
+export default class Contacts extends Component {
+  render() {
+    return this.props.contacts.map(contact => (
+      <li className={css.contactsList} key={contact.id}>{contact.name}</li>
+    ));
+  }
+}
