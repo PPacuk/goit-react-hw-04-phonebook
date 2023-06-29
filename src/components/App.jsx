3dfadea82bc7d8954d5162a29123a4983ecd9ddb
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-06-29 22:23:36 +0200

    refractor done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,10 +11,6 @@ import css from './App.module.css';
 
 export const App = () => {
   const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
   const [filter, setFilter] = useState('');
 
@@ -40,13 +36,11 @@ export const App = () => {
   };
 
   const deleteContact = contactId => {
-    setContacts(
-      localStorage.setItem(
-        'phonebook',
-        JSON.stringify(contacts.filter(contact => contact.id !== contactId))
-      )
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+    localStorage.setItem(
+      'phonebook',
+      JSON.stringify(contacts.filter(contact => contact.id !== contactId))
     );
-    console.log(JSON.stringify(contacts.filter(contact => contact.id !== contactId)));
   };
 
   const searchInputHandler = e => {
